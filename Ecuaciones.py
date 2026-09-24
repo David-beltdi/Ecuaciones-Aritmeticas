6eee9ad9e27d89350309cf88d7c720f54620d967
David-beltdi <juandadiazb22@gmail.com>
2026-09-23 23:23:56 -0500

    Ecuaciones sencillas

---
+++ Ecuaciones.py
@@ -0,0 +1,38 @@
+print("OPERACIONES ESENCIALES CON PYTHON")
+print("Datos iniciales")
+a=10
+b=5
+print("a=10")
+print("b=5")
+print("SUMA")
+print("La suma de 10 mas 5 es:")
+resultado=a+b
+print(resultado)
+print("RESTA")
+print("La resta de 10 menos 5 es:")
+resultado=a-b
+print(resultado)
+print("MULTIPLICACION")
+print("La multiplicacion de 10 por 5 es:")
+resultado=a*b
+print(resultado)
+print("Nuevos datos")
+b=4
+print("b=4")
+print("DIVISION")
+print("La division exacta de 10 entre 4 es:")
+resultado=a/b
+print(resultado)
+print("DIVISION ENTERA")
+print("El entero de la division de 10 entre 4 es:")
+resultado=a//b
+print(resultado)
+print("Nuevos datos")
+a=2
+b=3
+print("a=2")
+print("b=3")
+print("POTENCIA")
+print("2 elevado a la potencia de 3 da como resultado:")
+resultado=a**b
+print(resultado)
